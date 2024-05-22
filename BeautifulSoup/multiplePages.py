fbbc139157f3b89548c62739875e6c4f7046a046
Marcelo Casa <mppozzobon8@gmail.com>
2024-05-22 10:57:51 -0300

    multiplePages.py commit

--- BeautifulSoup/multiplePages.py
+++ BeautifulSoup/multiplePages.py
@@ -3,4 +3,29 @@ import requests
 import time
 
 website = 'https://subslikescript.com/movies'
-response = requests.get(website)
+
+try:
+    response = requests.get(website)
+    content = response.text
+
+    soup = BeautifulSoup(content, 'lxml')
+
+    article = soup.find('article', class_='main-article')
+
+    links = []
+
+    for link in article.find_all('a', href=True):
+        links.append(link['href'])
+        time.sleep(1)
+
+    print(links)
+
+
+    """ for link in links:
+        time.sleep(1) """
+except requests.exceptions.RequestException as e:
+    print(f'Erro ao fazer a request: {e}')
+except AttributeError as e:
+    print(f'Erro de atribuição: {e}')
+except Exception as e:
+    print(f'Exceção: {e}')
